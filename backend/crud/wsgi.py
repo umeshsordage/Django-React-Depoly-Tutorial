7b4cd92860731d15fa1f636c76592dedc9daac40
umeshsordage <soradgeumesh1996@gmail.com>
2024-02-25 02:39:33 +0530

    change to about page

--- backend/crud/wsgi.py
+++ backend/crud/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+settings_module = 'crud.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'crud.settings'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crud.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
